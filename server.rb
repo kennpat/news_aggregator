8d298b1984501d812648ac4629abbfb365bf37ab
kennpat <kennedypr@gmail.com>
2014-11-20 16:23:36 -0500

    Added the ability to add an article which will be archived in the csv

--- server.rb
+++ server.rb
@@ -16,7 +16,7 @@ def read_news(filename)
 			article_link: row['article_link'],
 			article_summary: row['article_summary']
 		}
-		binding.pry
+		#binding.pry
 	end
 	news_arr
 end
@@ -24,7 +24,7 @@ end
 def save_news(filename, article_title, article_link, article_summary)
 	time  = Time.new
 	CSV.open( filename, 'a' ) do |csv|
-		csv.instert(0, [article_title, time, article_link, article_summary])
+		csv << [article_title, time, article_link, article_summary]
 	end
 end
 
@@ -37,8 +37,32 @@ get '/' do
 end
 
 get '/new' do
-
 	erb :new
 end
 
-#puts
+#posts
+
+post '/new' do
+	#hash structure - refer to above function to read the csv file
+
+	@title = params[:article][:name]
+	@link = params[:article][:link]
+	@summary = params[:article][:summary]
+	#binding.pry
+	save_news('news.csv', @title, @link, @summary)
+	# if !params[:article_title].empty? && params[:article_link].empty? && params[:article_summary].empty?
+	# 	#add the article
+	# 	save_news('news.csv', @title, @link, @summary)
+
+	# 	redirect '/'
+	# else
+	# 	@error_message = []
+	# 	@error_message << "You must enter a title." if params[:article_title].empty?
+	# 	@error_message << "You must enter a link." if params[:article_link].empty?
+	# 	@error_message << "You forgot to add a summary to the story" if params[:article_summary].empty?
+	@news = read_news('news.csv')
+	erb :index
+	# end
+	
+end
+
